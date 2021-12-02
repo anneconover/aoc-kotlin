c7cbc99e92108593f27f849ce042a3988f9a54a2
anneconover <anne.conover@onepeloton.com>
2021-12-02 00:04:01 -0500

    test file

---
+++ src/Day02/Day02.kt
@@ -0,0 +1,26 @@
+package Day02
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val a = input.map { it.toInt() }
+        return a.zipWithNext().count { (x, y) -> y > x }
+    }
+
+//    fun part2(input: List<String>): Int {
+//        val a = input.map { it.toInt() }
+//        return a.windowed(3).map { it.sum() }.zipWithNext().count {
+//                (x, y) -> y > x
+//        }
+//    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02/Day02_test")
+    check(part1(testInput) == 7)
+//    check(part2(testInput) == 5)
+
+//    val input = readInput("Day02/Day02")
+//    println(part1(input))
+//    println(part2(input))
+}
