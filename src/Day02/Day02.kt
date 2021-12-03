95e38573215e68a54d32a7b0a8ed77c50e620d40
anneconover <anne.conover@onepeloton.com>
2021-12-02 22:30:36 -0500

    TDD test should fail

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -28,17 +28,31 @@ fun main() {
         return(horizontal * vertical)
     }
 
-//    fun part2(input: List<String>): Int {
-//        val a = input.map { it.toInt() }
-//        return a.windowed(3).map { it.sum() }.zipWithNext().count {
-//                (x, y) -> y > x
-//        }
-//    }
+    fun part2(input: List<String>): Int {
+        var horizontal = 0
+        var vertical = 0
+
+        for (line in input) {
+            val (command, value) = line.split(" ")
+            when (command) {
+                FORWARD -> {
+                    horizontal += value.toInt()
+                }
+                UP -> {
+                    vertical -= value.toInt()
+                }
+                DOWN -> {
+                    vertical += value.toInt()
+                }
+            }
+        }
+        return(horizontal * vertical)
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02/Day02_test")
     check(part1(testInput) == 150)
-//    check(part2(testInput) == 5)
+    check(part2(testInput) == 60)
 
     val input = readInput("Day02/Day02")
     println(part1(input))
